de4b8a3342c68774febfed09769bd7b81e931fcb
You Song Hou <yousonghou@yahoo.com>
2020-04-28 23:32:10 -0400

    move some files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route  } from 'react-router-dom';
 import './App.css';
-import { getSampleData } from './api';
 
 import 'leaflet/dist/leaflet.css';
 
-import Area from './components/Area';
-import Line from './components/Line';
+import Charts from './components/Charts/Charts';
 import MapUS from './components/MapUS';
 
 class App extends Component{
-  state = {
-    resData: {
-      dates: [],
-      source: [],
-      volume: []
-    }
-  }
-  async componentDidMount(){
-    const data = await getSampleData();
-
-    this.setState({ resData: data });
-  }
-
   render(){
     return (
       <Router>
         <h1 className="center-align">Tweet Data</h1>
+        <Route exact path="/" component={Charts} />
         <MapUS />
-        <Area dates={this.state.resData.dates} data={this.state.resData.source} />
-        <Line dates={this.state.resData.dates} data={this.state.resData.volume} />
       </Router>
     );
   }
---
+++ src/components/Charts/Charts.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { getSampleData } from '../../api';
+
+import Area from './Area';
+import Line from './Line';
+
+class Charts extends Component{
+  state = {
+    resData: {
+      dates: [],
+      source: [],
+      volume: []
+    }
+  }
+  async componentDidMount(){
+    const data = await getSampleData();
+
+    this.setState({ resData: data });
+  }
+
+  render(){
+    return (
+      <div>
+        <Area dates={this.state.resData.dates} data={this.state.resData.source} />
+        <Line dates={this.state.resData.dates} data={this.state.resData.volume} />
+      </div>
+    );
+  }
+}
+
+export default Charts;
