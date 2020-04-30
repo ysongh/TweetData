c7d08e42e4e73744f58ea103d880831d35bdb5a0
You Song Hou <yousonghou@yahoo.com>
2020-04-30 01:42:20 -0400

    add a card to show the top 5 symptoms

--- src/components/Symptom.js
+++ src/components/Symptom.js
@@ -15,17 +15,40 @@ class Symptom extends Component{
     return (
       <div className="container">
         <h1 className="center-align">Symptom</h1>
-        <div className="card">
-            <div className="card-content">
-                {this.state.data ? this.state.data.map((i) => {
-                    return (
-                        <div className="chip" key={i.name}>
-                            {i.name} {i.value}
-                        </div>
-                    )
-                }) : null}
+        <div className="row">
+            <div className="card col s12 m6">
+                <ul className="collection with-header">
+                    <li className="collection-header"><h4>Top 5 Symptom</h4></li>
+                    <li className="collection-item">
+                        <span>cold</span><span className="right">123</span>
+                    </li>
+                    <li className="collection-item">
+                        <span>cold</span><span className="right">123</span>
+                    </li>
+                    <li className="collection-item">
+                        <span>cold</span><span className="right">123</span>
+                    </li>
+                    <li className="collection-item">
+                        <span>cold</span><span className="right">123</span>
+                    </li>
+                    <li className="collection-item">
+                        <span>cold</span><span className="right">123</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="card col s12">
+                <div className="card-content">
+                    {this.state.data ? this.state.data.map((i) => {
+                        return (
+                            <div className="chip" key={i.name}>
+                                {i.name} {i.value}
+                            </div>
+                        )
+                    }) : null}
+                </div>
             </div>
         </div>
+        
       </div>
     );
   }
