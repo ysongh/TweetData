465d07d9002afc7dd4917b353ef288c9680b2182
You Song Hou <yousonghou@yahoo.com>
2020-04-30 18:08:38 -0400

    move some functions to helper.js

---
+++ src/api/helper.js
@@ -0,0 +1,58 @@
+export const objectToArray =  (newData) => {
+    let total = 0;
+    // remove the date from the object
+    delete newData[0].date;
+
+    // convert object into array of ojects
+    const list = [];
+    let dataAr = new Map(Object.entries(newData));
+    dataAr = Object.fromEntries(dataAr);
+    for(let i in dataAr[0]){
+        // only show symptom that does not have value that are null
+        if(dataAr[0][i]){
+            total += dataAr[0][i];
+            list.push({
+                name: i,
+                value: dataAr[0][i]
+            })
+        }
+    }
+
+    return {total, list};
+}
+
+export const donutData = (top5, total) => {
+    let count = 0;
+    let labels = []
+    let values = [];
+
+    for(let i of top5){
+        count += i.value;
+        labels.push(i.name.toUpperCase());
+        values.push(i.value);
+    }
+    labels.push("OTHER");
+    values.push(total - count);
+
+    return { labels, values };
+}
+
+/*
+const removeProperty = obj => {
+    const newObj = obj.map(i => {
+        if(i.ayoikutiaturan){
+            console.log(i.ayoikutiaturan)
+        }
+        delete i.country_region_code;
+        delete i.parks_percent_change_from_baseline;
+        delete i.grocery_and_pharmacy_percent_change_from_baseline;
+        delete i.residential_percent_change_from_baseline;
+        delete i.retail_and_recreation_percent_change_from_baseline;
+        delete i.transit_stations_percent_change_from_baseline;
+        delete i.workplaces_percent_change_from_baseline;
+        return i;
+    });
+
+    return newObj;
+}
+*/
--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import { objectToArray, donutData } from './helper';
+
 const url1 = "https://raw.githubusercontent.com/yenlow/tmp_funicular/b8ad9f5a1c4091ca7e79a0c5f09e550b17194202/data/sample_data_for_mockup.json";
 const url2 = "https://raw.githubusercontent.com/yenlow/tmp_funicular/1f5720396ad531e305fe3793b4c243a414533788/data/sample_jhu_data_for_mockup.json";
 
@@ -125,62 +127,3 @@ export const getTypeData = async (url, date) => {
         console.log(error);
     }
 }
-
-const objectToArray =  (newData) => {
-    let total = 0;
-    // remove the date from the object
-    delete newData[0].date;
-
-    // convert object into array of ojects
-    const list = [];
-    let dataAr = new Map(Object.entries(newData));
-    dataAr = Object.fromEntries(dataAr);
-    for(let i in dataAr[0]){
-        // only show symptom that does not have value that are null
-        if(dataAr[0][i]){
-            total += dataAr[0][i];
-            list.push({
-                name: i,
-                value: dataAr[0][i]
-            })
-        }
-    }
-
-    return {total, list};
-}
-
-const donutData = (top5, total) => {
-    let count = 0;
-    let labels = []
-    let values = [];
-
-    for(let i of top5){
-        count += i.value;
-        labels.push(i.name.toUpperCase());
-        values.push(i.value);
-    }
-    labels.push("OTHER");
-    values.push(total - count);
-
-    return { labels, values };
-}
-
-/*
-const removeProperty = obj => {
-    const newObj = obj.map(i => {
-        if(i.ayoikutiaturan){
-            console.log(i.ayoikutiaturan)
-        }
-        delete i.country_region_code;
-        delete i.parks_percent_change_from_baseline;
-        delete i.grocery_and_pharmacy_percent_change_from_baseline;
-        delete i.residential_percent_change_from_baseline;
-        delete i.retail_and_recreation_percent_change_from_baseline;
-        delete i.transit_stations_percent_change_from_baseline;
-        delete i.workplaces_percent_change_from_baseline;
-        return i;
-    });
-
-    return newObj;
-}
-*/
